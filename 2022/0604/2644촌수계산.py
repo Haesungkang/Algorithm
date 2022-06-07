d153cf5c5a687ae9bfc24e172d8782cf16efab24
haesungkang <kangs940909@gmail.com>
2022-06-07 22:35:50 +0900

    June 7th Study

--- 2022/0604/2644촌수계산.py
+++ 2022/0604/2644촌수계산.py
@@ -10,19 +10,39 @@ for i in range(node):
     arr[i][j] = arr[j][i] = 1
 
 visited = [0] * (n+1)
-cnt = 0
+result = []
+result.append(a)
 def dfs(x):
-    global cnt
     visited[x] = 1
     if x == b:
         return 
-    for w in range(n+1):
-        if arr[x][w] == 1 and visited[w] == 0:
-            cnt += 1
-            dfs(w)
-    cnt = 0 
+    else:
+        for w in range(n+1):
+            if arr[x][w] == 1 and visited[w] == 0:
+                result.append(w)
+                dfs(w)
 
 dfs(a)
-print(cnt)
+#print(result)
+chon1 = 0
+chon2 = 0
+cnt = 0
+for i in range(len(result)):
+    if result[i] == a:
+        cnt += 1
+        chon1 = i
+
+for j in range(len(result)):
+    if result[j] == b:
+        cnt += 1
+        chon2 = j
+
+#print(chon1, chon2)
+
+if abs(chon1-chon2) > 0 and cnt == 2:
+    print(abs(chon1-chon2))
+else:
+    print(-1)
+
 
 
